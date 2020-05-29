dc92c9d4b5c50273c1f3f278953c1621d4f0dc63
felipebattistotti <feligatti@gmail.com>
2020-05-28 23:11:06 -0300

    Commit new features

---
+++ src/pages/contato.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/header';
+
+const Contato = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Contatos</h1>
+        </div>
+    )
+};
+
+export default Contato;
